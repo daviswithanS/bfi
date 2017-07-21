f8e79707f9c519a85088ed37a430fa9a92dc41fe
daviswithanS <davis.defoor@gmail.com>
2017-07-21 00:06:45 -0400

    Remove Instructions object.
    This iteration has been the first tradeoff that I have made
    between storage and speed. I removed the Instructions object,
    which stripped the commands of comments, loaded the commands
    into  a vector, and iterated back and forth over them. Now, the
    program simply gets and puts back each character from the file.
    While this approach results in a smaller file size for the
    interpreter, it runs much slower: comments are no longer
    stripped, and, although I do not know how c++ handles file
    streams, the repeated calls of get() and unget() are probably
    not helping much. However, since I am prioritizing storage
    space, this is the right call. (I might put back the vector
    later and see what changes though)

--- bf.cpp
+++ bf.cpp
@@ -1,102 +1,19 @@
-#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <stack>
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include "bf.h"
 
-using std::size_t;
 using std::ifstream;
 using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::stack;
-using std::invalid_argument;
 using std::out_of_range;
 using std::string;
 using std::vector;
-
-Interpreter::Interpreter(const string& filename)
-{
-	load_instructions(filename); 
-	iter = commands.begin();
-}
-
-// The load_instructions function parses an input file and stores all of the
-// valid brainfuck commands.
-void Interpreter::load_instructions(const string& filename)
-{
-	ifstream file(filename.c_str());
-	if (!file) {
-		throw invalid_argument("invalid or missing filename");
-	}
-
-	char c;
-	while (file.get(c)) {		// loop ends if eof or error
-		if (is_valid(c)) {
-			commands.push_back(c);
-		}
-	}
-	commands.push_back('\0');
-}
-
-bool Interpreter::is_valid(const char c)
-{
-	if (c == '>' || c == '<' || c == '+' || c == '-' ||
-			c == '.' || c == ',' || c == '[' || c == ']') {
-		return true;
-	}
-	return false;
-}
-
-void Interpreter::jumpf()
-{
-	stack<char> brackets;
-	brackets.push('[');
-	// go until end is reached or all brackets are resolved (a pair is found)
-	while (iter != commands.end()) {
-		++iter;
-		switch (*iter) {
-			case '[': brackets.push('['); break;
-			case ']': brackets.pop(); break;
-		}
-
-		if (brackets.size() == 0) {
-			break;
-		}
-	}
-
-	// throw error if any brackets are unresolved
-	if (brackets.size() > 0) {
-		throw out_of_range("program had an unclosed '['");
-	}
-}
-
-void Interpreter::jumpb()
-{
-	stack<char> brackets;
-	brackets.push(']');
-	// see jumpf()
-	while (iter != commands.begin()) {
-		--iter;
-		switch (*iter) {
-			case ']': brackets.push(']'); break;
-			case '[': brackets.pop(); break;
-		}
-
-		if (brackets.size() == 0) {
-			break;
-		}
-	}
-
-	// throw error if any brackets are unresolved
-	if (brackets.size() > 0) {
-		throw out_of_range("program had an unclosed ']'");
-	}
-}
 
 int main(int argc, char** argv)
 {
@@ -105,7 +22,11 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	Interpreter in(argv[1]);
+	ifstream file(argv[1]);
+	if (!file) {
+		cerr << "invalid or missing filename" << endl;
+		return 1;
+	}
 
 	vector<unsigned char>::size_type size = 10;
 
@@ -114,14 +35,14 @@ int main(int argc, char** argv)
 	vector<unsigned char> cells(size, 0);
 	vector<unsigned char>::size_type index = 0;
 
-	while (in.current() != '\0') {
-		char command = in.current();
-
+	char command;
+	while (file.get(command)) {
 		switch (command) {
 			// move right
 			case '>': 
 				if (index == cells.max_size()-1) {
-					throw out_of_range("program tried to access too large of a cell");
+					cerr << "program tried to access too large of a cell" << endl;
+					return 1;
 				}
 				++index;
 				// add more cells if necessary
@@ -132,7 +53,8 @@ int main(int argc, char** argv)
 			// move left
 			case '<': 
 				if (index == 0) {
-					throw out_of_range("program tried to access cell -1");
+					cerr << "program tried to access cell -1" << endl;
+					return 1;
 				}
 				--index;
 				break;
@@ -159,18 +81,52 @@ int main(int argc, char** argv)
 			// loop begin
 			case '[': 
 				if (cells[index] == 0) {
-					in.jumpf();
+					stack<char> brackets;
+					brackets.push('[');
+					// go until end is reached or all brackets are resolved (a pair is found)
+					while (file.get(command)) {
+						switch (command) {
+							case '[': brackets.push('['); break;
+							case ']': brackets.pop(); break;
+						}
+
+						if (brackets.size() == 0) {
+							break;
+						}
+					}
+
+					// throw error if any brackets are unresolved
+					if (brackets.size() > 0) {
+						cerr << ("program had an unclosed '['") << endl;
+						return 1;
+					}
 				}
 				break;
 			// loop end
 			case ']':
 				if (cells[index] != 0) {
-					in.jumpb();
+					stack<char> brackets;
+					// see jumpf()
+					while (file.unget()) {
+						command = file.peek();
+						switch (command) {
+							case ']': brackets.push(']'); break;
+							case '[': brackets.pop(); break;
+						}
+
+						if (brackets.size() == 0) {
+							break;
+						}
+					}
+
+					// throw error if any brackets are unresolved
+					if (brackets.size() > 0) {
+						cerr << "program had an unclosed ']'" << endl;
+						return 1;
+					}
 				}
 				break;
 		}
-		in.next();
 	}
-
 	return 0;
 }	
