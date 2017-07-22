758eb0dbcc87e2627bd4317676be99d20f564d02
daviswithanS <davis.defoor@gmail.com>
2017-07-22 15:01:28 -0400

    Load instructions before interpreting.
    Instead of reading from the file directly, the
    interpreter now loads all of the instructions
    into a vector and loops through the vector to get
    the commands. This is faster for two reasons: one,
    the program does not have to keep reading from the
    file, and two, it can remove all the non-commands
    as it loads them in, removing the need for looping
    over comments.

--- bf.cpp
+++ bf.cpp
@@ -23,106 +23,108 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	// load instructions and clear all non-brainfuck commands
+	vector<char> instructions;
+	char command;
+	while (file.get(command)) {
+		if (   command == '>' || command == '<'
+	 	    || command == '+' || command == '-'
+		    || command == '.' || command == ',' 
+		    || command == '[' || command == ']') {
+			instructions.push_back(command);
+		}
+	}
+	vector<char>::const_iterator current = instructions.begin();
+
 	// represents the "address field" - a vector of cells from 0 to
 	// infinity, where each cell stores a number up to 255
 	vector<unsigned char> cells(3000, 0);
 	vector<unsigned char>::size_type index = 0;
 
-	char command;
-	while (file.get(command)) {
-		switch (command) {
-			// move right
-			case '>': 
-				if (index == cells.max_size()-1) {
-					cerr << "program tried to access too large of a cell" << endl;
+	while (current != instructions.end()) {
+		switch (*current) {
+		case '>': 
+			if (index == cells.max_size()-1) {
+				cerr << "program tried to access too large of a cell" << endl;
+				return 1;
+			}
+			++index;
+			// add more cells if necessary
+			if (index >= cells.size()) {
+				cells.push_back(0);
+			}
+			break;
+		case '<': 
+			if (index == 0) {
+				cerr << "program tried to access cell -1" << endl;
+				return 1;
+			}
+			--index;
+			break;
+		case '+': 
+			++cells[index];
+			break;
+		case '-': 
+			--cells[index];
+			break;
+		case '.': 
+			cout << cells[index];
+			break;
+		case ',': 
+			char c;
+			if (!cin.get(c)) {
+				c = 0;
+			}
+			cells[index] = c;
+			break;
+		case '[': 
+			if (cells[index] == 0) {
+				vector<unsigned char>::size_type brackets = 0;
+
+				// stops going forward at eof or when brackets is 0
+				while (current != instructions.end()) {
+					switch (*current) {
+					case '[': ++brackets; break;
+					case ']': --brackets; break;
+					}
+
+
+					if (brackets == 0) {
+						break;
+					}
+					++current;
+				}
+
+				if (brackets > 0) {
+					cerr << ("program had an unclosed '['") << endl;
 					return 1;
 				}
-				++index;
-				// add more cells if necessary
-				if (index >= cells.size()) {
-					cells.push_back(0);
+			}
+			break;
+		case ']':
+			if (cells[index] != 0) {
+				vector<unsigned char>::size_type brackets = 0;
+
+				while (current != instructions.begin()) {
+					switch (*current) {
+					case ']': ++brackets; break;
+					case '[': --brackets; break;
+					}
+
+					if (brackets == 0) {
+						break;
+					}
+					--current;
 				}
-				break;
-			// move left
-			case '<': 
-				if (index == 0) {
-					cerr << "program tried to access cell -1" << endl;
+
+				if (brackets > 0) {
+					cerr << "program had an unclosed ']'" << endl;
 					return 1;
 				}
-				--index;
-				break;
-			// increment
-			case '+': 
-				++cells[index];
-				break;
-			// decrement
-			case '-': 
-				--cells[index];
-				break;
-			// write
-			case '.': 
-				cout << cells[index];
-				break;
-			// read
-			case ',': 
-				char c;
-				if (!cin.get(c)) {
-					c = 0;
-				}
-				cells[index] = c;
-				break;
-			// loop begin
-			case '[': 
-				if (cells[index] == 0) {
-					// initially set to 1 to include the starting bracket
-					vector<unsigned char>::size_type brackets = 1;
-
-					// stops going forward at eof or when brackets is 0
-					while (file.get(command)) {
-						switch (command) {
-							case '[': ++brackets; break;
-							case ']': --brackets; break;
-						}
-
-						if (brackets == 0) {
-							break;
-						}
-					}
-
-					if (brackets > 0) {
-						cerr << ("program had an unclosed '['") << endl;
-						return 1;
-					}
-				}
-				break;
-			// loop end
-			case ']':
-				if (cells[index] != 0) {
-					// initially set to 0 because unget() goes over the
-					// starting bracket again
-					vector<unsigned char>::size_type brackets = 0;
-
-					// stops going backward at beginning of file 
-					// (error returns false) or when brackets is 0
-					while (file.unget()) {
-						command = file.peek();
-						switch (command) {
-							case ']': ++brackets; break;
-							case '[': --brackets; break;
-						}
-
-						if (brackets == 0) {
-							break;
-						}
-					}
-
-					if (brackets > 0) {
-						cerr << "program had an unclosed ']'" << endl;
-						return 1;
-					}
-				}
-				break;
+			}
+			break;
 		}
+		++current;
 	}
 	return 0;
 }	
